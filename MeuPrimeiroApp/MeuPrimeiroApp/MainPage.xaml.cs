fcb2194b5a7f4d90c8e21b75b75e204e3af101f5
Samuel Soares <34168199+samuelsoares27@users.noreply.github.com>
2020-06-25 09:57:06 -0300

    Adicionar arquivos de projeto.

---
+++ MeuPrimeiroApp/MeuPrimeiroApp/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MeuPrimeiroApp
+{
+
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        List<String> itens = new List<String>();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected void OnClicked(object sender, EventArgs e)
+        {
+            string Nome = "";
+            string Email = "";
+
+            if (this.NomeCliente.Text != null)
+            {
+                Nome = this.NomeCliente.Text;
+            }
+            if (this.EmailCliente.Text != null)
+            {
+                Email = this.EmailCliente.Text;
+            }
+
+            if ((Nome != "") && (Email != ""))
+            {
+                             
+                itens.Add(Nome);
+
+                Lista.ItemsSource = "";
+                Lista.ItemsSource = itens;
+            }
+            else
+            {
+                DisplayAlert("Alerta", "Campos vazios", "Ok");
+            }
+
+        }
+    }
+
+}
